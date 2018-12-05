00e4519b87968a510b6096ec585646596d0dacee
aQaTL <mmsoltys@outlook.com>
2018-12-05 07:13:41 +0100

    Day5 optimalization (thanks @haaawk)

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,20 +1,20 @@
 fn main() {
 	let input = std::fs::read("input.txt").unwrap();
-	println!("Part 1: {}", part_1(&input));
-	println!("Part 2: {}", part_2(&input));
+	println!("Part 1: {}", part_1(input.clone()));
+	println!("Part 2: {}", part_2(input));
 }
 
-fn part_1(input: &Vec<u8>) -> usize {
-	let mut input = input.clone();
+fn part_1(mut input: Vec<u8>) -> usize {
 	react(&mut input);
 	input.len()
 }
 
-fn part_2(input: &Vec<u8>) -> usize {
+fn part_2(mut input: Vec<u8>) -> usize {
 	let mut best_react = usize::max_value();
+	react(&mut input);
 	for c in 65..=90 {
 		let mut input_2 = input.iter()
-			.filter(|&&x| x != c && x != (c + 32))
+			.filter(|&&x| x != c && x != c + 32)
 			.map(|x| x.clone())
 			.collect::<Vec<_>>();
 		react(&mut input_2);
@@ -28,7 +28,7 @@ fn part_2(input: &Vec<u8>) -> usize {
 fn react(input: &mut Vec<u8>) {
 	'l: loop {
 		for i in 0..input.len() - 1 {
-			if input[i] == (input[i + 1] + 32) || (input[i] + 32) == input[i + 1] {
+			if input[i] == input[i + 1] + 32 || input[i] + 32 == input[i + 1] {
 				input.remove(i);
 				input.remove(i);
 				continue 'l;

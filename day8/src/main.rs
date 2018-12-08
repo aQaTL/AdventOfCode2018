93bc18871d79c42985e6c5c2e1d1220ace702bb4
aQaTL <mmsoltys@outlook.com>
2018-12-08 08:32:10 +0100

    Day 8 refactor

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,12 +4,10 @@ fn main() {
 	let input: Vec<_> = std::fs::read_to_string("input.txt").unwrap()
 		.split(" ").map(|x| x.parse::<u32>().unwrap()).collect();
 
-	let (mut i, mut metadata_sum) = (0usize, 0u32);
+	let (mut i, mut metadata_sum, mut last_node_value) = (0usize, 0u32, 0u32);
 	let mut queue: Vec<Node> = Vec::with_capacity(input.len());
-	while i < input.len() {
-		if queue.len() == 0 {
-			push_node(&input, &mut i, &mut queue);
-		}
+	push_node(&input, &mut i, &mut queue);
+	loop {
 		let tail = queue.last_mut().unwrap();
 		if tail.children as usize == tail.child_values.len() {
 			if tail.children == 0 {
@@ -17,31 +15,25 @@ fn main() {
 					metadata_sum += input[i];
 					tail.value += input[i];
 				}
-				i += tail.metadata as usize;
-				let val = tail.value;
-				queue.pop();
-				if let Some(last_last) = queue.last_mut() {
-					(*last_last).child_values.push(val);
-				}
 			} else {
 				for i in i..i + tail.metadata as usize {
 					metadata_sum += input[i];
 					tail.value += tail.child_values.get((input[i] - 1) as usize).unwrap_or(&0);
 				}
-				i += tail.metadata as usize;
-				let val = tail.value;
-				queue.pop();
-				if let Some(last_last) = queue.last_mut() {
-					(*last_last).child_values.push(val);
-				} else {
-					println!("Part 1: {}", metadata_sum);
-					println!("Part 2: {}", val);
-				}
+			}
+			i += tail.metadata as usize;
+			last_node_value = tail.value;
+			queue.pop();
+			match queue.last_mut() {
+				Some(last_last) => (*last_last).child_values.push(last_node_value),
+				None => break,
 			}
 		} else {
 			push_node(&input, &mut i, &mut queue);
 		}
 	}
+	println!("Part 1: {}", metadata_sum);
+	println!("Part 2: {}", last_node_value);
 }
 
 fn push_node(input: &Vec<u32>, i: &mut usize, queue: &mut Vec<Node>) {
